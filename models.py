042f2f3950a88e169fd8b765be9836ebfacc4552
nirav9426 <nirav.lad@netweb.biz>
2026-04-14 18:10:26 +0530

    Add database models

---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    venue = db.Column(db.String(120), nullable=False)
+    
+    def __repr__(self):
+        return f'<Event {self.name}>'
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Ticket {self.id}>'
